dd18fa8280128de58e978b96f179bf265cee00ae
Nelson <nels.fomekong@gmail.com>
2021-11-17 13:03:44 +0100

    premier commit sur master

---
+++ src/main/java/org/sid/esgialcc1/EsgiAlCc1Application.java
@@ -0,0 +1,13 @@
+package org.sid.esgialcc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsgiAlCc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsgiAlCc1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/sid/esgialcc1/EsgiAlCc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.esgialcc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsgiAlCc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
